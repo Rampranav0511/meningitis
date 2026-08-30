bb4323f5b432a1af148cc42a112010fabdd3908f
Rampranav0511 <patibandla.rampranav@gmail.com>
2026-08-30 13:08:04 +0530

    edited the file

--- apicom.py
+++ apicom.py
@@ -3,7 +3,8 @@ import chess
 # requests is a library which sends requests to server , it handles all the socket connection and stufff
 # you could code a part of this for your util on your own
 user= "Rampranav0511"
-oldgames=requests.get(f"https://api.chess.com/pub/player{user}/games/archives") # this is basically a url
+head={"User-agent":"chesseval (contact: haha@gmail.com"}
+oldgames=requests.get(f"https://api.chess.com/pub/player/{user}/games/archives") # this is basically a url
 # get function sends a http get request to that url requesting for data , in this case - archive games
 
 data=oldgames.json()
@@ -13,7 +14,7 @@ this contains header , metadata and some content , including status_code,  ok ch
 l=[]
 urls=data["archives"] # archives key holds a collection of links (url's) to my one month games (jan,feb,march.....)
 for url in urls:# accessing one particular month every loop
-    games=requests.get(url)
+    games=requests.get(url,headers=head)
     gamesdata=games.json() # contains jan,feb,march , dictionaries
     
     for game in gamesdata["games"]:
@@ -25,7 +26,7 @@ for url in urls:# accessing one particular month every loop
     #pgobject=chess.game.read_game(ele) # this function takes in only a file as an input , so we need to write all pgn's into a file
 with open("mygames.pgn","w") as f:
     for ele in l:
-        f.write(ele+"/n/n") # blank line after each game
+        f.write(ele+"\n\n") # blank line after each game
 # f.close() , we used with open here so it does it automatically
 
 
