12ba9d9bae613cd515fdaa4ee4a734efa8bb18a9
Rampranav0511 <patibandla.rampranav@gmail.com>
2026-08-30 12:49:58 +0530

    minor tweaks in the network

--- fakeneural.py
+++ fakeneural.py
@@ -32,28 +32,37 @@ def forward_pass(inp,weig,bia):
 
 
 
-dimensions=[input_no] + outputsizes
+dimensions=[input_no] + outputsizes # this gives you the layer by layer input neurons
 """
 first_act=forward_pass(input_layer,first_weights,first_bias)
 # act is now the final output in the last layer , the last layer here has only one neuron btw
 first_out=forward_pass(first_act,weights,bias) """
-for i in range(0,len(outputsizes)-1):
+for i in range(0,len(dimensions)-1):
     
     
     weights=n.random.randn(dimensions[i+1],dimensions[i])# each thing is the weights matrix per layer
     bias=n.random.randn(dimensions[i+1],1) # bias aswelll
     weight_list.append(weights)
     bias_list.append(bias)
-    
+batchsize=64
+
 total_epochs=100
 best_val_loss=float('inf')
 patience=5
 counter=0
 for e in range(total_epochs):
 
+    for i in range(0,len(tr),batchsize):
+                           
+        batch=tr[i:i+batchsize]                             
+        if len(batch)<2:                                     
+            continue
+        pos=n.hstack([x.reshape(-1,1) for x,y in batch])    # horizontally stacks input layers(coluwn wise adding)            
+        evaltarget=n.array([y for x,y in batch]).reshape(1,-1)
 
 
-    for pos,evaltarget in tr: #tr is basically the array of tuples ( each tuple is each position , eval)
+
+         #tr is basically the array of tuples ( each tuple is each position , eval)
         
 
         layer_acts=[]
@@ -77,7 +86,7 @@ for e in range(total_epochs):
 
             wog=weight_list[j]
             nudge=de@prev_act.T
-            biasnudge=de
+            biasnudge=de.sum(axis=1,keepdims=True)
 
             weight_list[j]=wog-step*nudge
             bias_list[j]-=step*biasnudge
@@ -85,13 +94,14 @@ for e in range(total_epochs):
             the output error is basically final activation - target , later on it gets
             multiplied with the derivative of activation function of previous layers"""
             if j!=0:
-                de=(wog.T@de)*(present_act*(1-present_act))
+                de=(wog.T@de)*(prev_act*(1-prev_act))
     # validation layer (forward pass of new examples but on updated weights in every epoch)
                 
     valloss=0
     for position,target in valdata:
-        act=position
+        act=position.reshape(-1,1)
         for i in range(len(weight_list)):
+
             act=weight_list[i]@act + bias_list[i]
             act=1/(1+n.exp(-act))
         valloss+=n.mean((act-target)**2)
@@ -102,6 +112,15 @@ for e in range(total_epochs):
     if avg_valloss < best_val_loss:
         best_val_loss=avg_valloss
         counter=0
+        dict={}
+
+        for index,(weih,bias) in enumerate(zip(weight_list,bias_list)):
+            dict[f'weih{index}']=weih
+            dict[f'bias{index}']=bias
+
+            """we basically ran a loop and saved all the weights 
+            in the dictionary and then we are saving the dictionary as a .npz file using savez in numpy"""
+        n.savez("neural network",*weight_list,*bias_list)    
     else:
         counter+=1
         if counter>=patience:
